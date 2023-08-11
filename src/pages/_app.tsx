959000e3af6a10b931947dc210da3123d2b136fe
Camila Silva <camilasilva@MACECOGN8SRQ6L4.local>
2023-08-11 09:48:26 -0300

    initial

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+/* eslint-disable filenames/match-regex */
+import "@telefonica/mistica/css/reset.css";
+import "@telefonica/mistica/css/roboto.css";
+import "@telefonica/mistica/css/mistica.css";
+
+import {
+  getVivoNewSkin,
+  ThemeConfig,
+  ThemeContextProvider,
+} from "@telefonica/mistica";
+
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const misticaTheme: ThemeConfig = {
+    skin: getVivoNewSkin(),
+    i18n: { locale: "pt-BR", phoneNumberFormattingRegionCode: "BR" },
+  };
+
+  return (
+    <ThemeContextProvider theme={misticaTheme}>
+      <Component {...pageProps} />
+    </ThemeContextProvider>
+  );
 }
